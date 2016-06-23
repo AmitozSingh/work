ccb94f66a60d9f22f8f37a01d25b817fddd3e486
Amitoz <amitoznarang1992@gmail.com>
2016-06-23 21:48:59 +0530

    updated file ReadExcelData

--- SeleniumFramework/src/ReadExcelData/ReadExcel.java
+++ SeleniumFramework/src/ReadExcelData/ReadExcel.java
@@ -2,41 +2,60 @@ package ReadExcelData;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadExcel {
-
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		File src=new File("C:\\Users\\TATA-OMNI\\workspace\\TestData\\TestData.xlsx");
-		FileInputStream fis= new FileInputStream(src);
-		XSSFWorkbook wb= new XSSFWorkbook(fis);
-		 XSSFSheet Sheet1=wb.getSheetAt(0);
-		String value=Sheet1.getRow(0).getCell(0).getStringCellValue();
-		int rowcount=Sheet1.getLastRowNum();
-		System.out.println(rowcount);
-		int Cellcount=0;
+	public static String[][] ReadExcel(String Path,int Sheetno) throws Exception 
+	{
+		XSSFWorkbook wb = null;
 		String[][]CellData=null;
-		for(int i=0;i<=rowcount;i++)
-		{
-			System.out.println(i);
-			Cellcount=Sheet1.getRow(i).getLastCellNum();
-			for(int j=0;j<Cellcount;j++)
+		try {
+		
+			File src=new File(Path);
+			FileInputStream fis= new FileInputStream(src);
+			wb = new XSSFWorkbook(fis);
+			 XSSFSheet Sheet1=wb.getSheetAt(Sheetno);
+			
+			int rowcount=Sheet1.getPhysicalNumberOfRows();
+			System.out.println("Row Count="+rowcount);
+			int Cellcount=0;
+			
+			for(int i=0;i<rowcount;i++)
 			{
-				System.out.println(j);
-				CellData=new String[rowcount][Cellcount];
-				System.out.println(Sheet1.getRow(i).getCell(j).getStringCellValue());
-				CellData[i][j]=Sheet1.getRow(i).getCell(j).getStringCellValue();
-				System.out.println(CellData[i][j]);
+				System.out.println(i);
+				Cellcount=Sheet1.getRow(i).getPhysicalNumberOfCells();
+				System.out.println(Cellcount);
+				for(int j=0;j<Cellcount;j++)
+				{
+					System.out.println(j);
+					CellData=new String[rowcount][Cellcount];
+					CellData[i][j]=Sheet1.getRow(i).getCell(j).getStringCellValue();
+					System.out.println(CellData[i][j]);
+				}
+				
 			}
 			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
 		wb.close();
-	}
+		return CellData;
+		}
+	
 	
 
+	public static void main(String[] args) throws Exception {
+		
+		String ExcelData[][]=ReadExcel("C:\\Users\\TATA-OMNI\\workspace\\TestData\\TestData.xlsx", 0);
+		System.out.println(ExcelData[0][0]);
+		
+		
+		
+		
+
+}
 }
